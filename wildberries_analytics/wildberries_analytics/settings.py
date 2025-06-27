7ff888509b3599ad8b9e3b2dd1133669db84dc6b
Roman Feoktistov <romafeoktistov942@gmail.com>
2025-06-27 19:10:00 +0300

    feat: update settings to use environment variables for SECRET_KEY and PostgreSQL database configuration

--- wildberries_analytics/wildberries_analytics/settings.py
+++ wildberries_analytics/wildberries_analytics/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "your-secret-key"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
@@ -52,8 +52,12 @@ WSGI_APPLICATION = "wildberries_analytics.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": os.getenv("POSTGRES_USER"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
